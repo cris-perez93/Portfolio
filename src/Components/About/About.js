2a801457231995773a4e2839d46b54745bd416fd
Cristian Pérez Orozco <cristian.h.perez.o@gmail.com>
2021-10-06 21:22:07 +0100

    añadiendo proyecto

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -34,9 +34,11 @@ const About = () => {
              data-aos-easing="linear"
               data-aos-duration="500">
                 <h2>About me</h2>
-                <p className="parrafo">I am Cristian! Frontend Developer Junior, Passionate about new technologies and web development and interactive design, React is my best ally to create new projects.
+                <p className="parrafo">I am Cristian! Frontend Developer Junior, Passionate about new technologies and web development and interactive design, React is my best ally to create new projects.<br/>
 
-                I have always been passionate about design in all its aspects, and discovering design adapted to web technology is quite rewarding. In the projects that I develop I try to be dynamic, responsive, intuitive and fast.
+                I have always been passionate about design in all its aspects, and discovering design adapted to web technology is quite rewarding.<br/>
+                 In the projects that I develop I try to be dynamic, responsive, intuitive and fast.
+                <br/>By the way, I love to draw!
                 </p>
             </div> 
     </div>
--- src/Components/Projects/CarouselProjects.js
+++ src/Components/Projects/CarouselProjects.js
@@ -1,9 +1,10 @@
 
 import Project1  from './../../Images/Pixelk.jpg';
-
+import Project2  from '../../Images/coches.jpg'
 import Project3  from './../../Images/Seguros.jpg';
 import Project4  from './../../Images/Ejemplo.jpg';
 import Project5  from './../../Images/Redux.jpg';
+
 
 import  './Carousel.css';
 import { Fragment, useEffect } from 'react';
@@ -34,14 +35,15 @@ const CarouselProjects = () => {
 
       <div className="container_project">
         <div className="container_image_project">
-          <h3>Crud de Productos</h3>
+          <h3>Crud Products</h3>
           <img className="projects_image" alt="project" src ={Project5}/>
          </div>
           <div className="hover_container">
             <h2>ReduxCrud</h2>
             <div className="details">
                 
-                <p>Proyect with React, Redux, React-Router, REST API, and Axios.Proyect with React, Redux, React-Router, REST API, and AxiosProyect with React, Redux, React-Router, REST API, and Axios</p>
+                <p>Proyect with React, Redux, React-Router, REST API, and Axios.
+                Redux is a predictable state container designed to help you write JavaScript apps that behave consistently across client, server, and native environments and are easy to test.</p>
                 <div className="icons">
                 
                 <a className="icon" href="https://github.com/cris-perez93/ProductsCrud" target="_blank" rel="noreferrer" ><i className="fab fa-github" aria-hidden="true"></i></a>
@@ -56,6 +58,34 @@ const CarouselProjects = () => {
           </div>
         </div>
 
+        <div className="container_project">
+           <div className="container_image_project">
+           <h3>Car Renting</h3>
+            <img className="projects_image" alt="project" src ={Project2}/>
+           </div>
+           <div className="hover_container">
+              <h2>RentCars</h2>
+              <div className="details">
+                
+                <p>
+                  Widget with search form with several fields.
+                  The cars available for rental are shown below.
+                  For this project I have used React-Redux for a better centralization of the state.
+                </p>
+                <div className="icons">
+              
+                <a className="icon" href="https://github.com/cris-perez93/rent-cars" target="_blank" rel="noreferrer"><i className="fab fa-github" aria-hidden="true"></i></a>
+                <a className="icon" href="https://cris-perez93.github.io/rent-cars/" target="_blank" rel="noreferrer"><p>Demo</p></a>
+              </div>
+              
+              </div>
+
+              
+              
+              
+            </div>
+          </div>
+
 
      
 
@@ -69,7 +99,8 @@ const CarouselProjects = () => {
               <h2>ApiFilms</h2>
               <div className="details">
                 
-                <p>Layout for a movie website, using React and Axios.Layout for a movie website, using React and Axios.Layout for a movie website, using React and Axios.</p>
+                <p>Layout for a movie website, using React and and axios for call API movies.
+                Using css flexbox and adding logic to the visualization of the score obtained according to critics. </p>
                 <div className="icons">
               
                 <a className="icon" href="https://github.com/cris-perez93/movies" target="_blank" rel="noreferrer"><i className="fab fa-github" aria-hidden="true"></i></a>
@@ -100,7 +131,7 @@ const CarouselProjects = () => {
               <h2>Proyecto Crud</h2>
               <div className="details">
                     
-                    <p>Project with a series of functionalities created with React, Context and React-Router,Project with a series of functionalities created with React, Context and React-Router</p>
+                    <p>Project with a series of functionalities created with React, ContextAPI and React-Router.The React Context API is a way for a React app to effectively produce global variables that can be passed around,it is an alternative to Redux for smaller projects</p>
                     <div className="icons">
                   
                     <a className="icon" href="https://github.com/cris-perez93/CrudProjects" target="_blank" rel="noreferrer" ><i className="fab fa-github" aria-hidden="true"></i></a>
@@ -125,10 +156,10 @@ const CarouselProjects = () => {
             </div>
             
             <div className="hover_container">
-            <h2>Presupuesto</h2>
+            <h2>Budget</h2>
             <div className="details">
                   
-                  <p>Small form to manage a weekly / monthly budget, adding and subtracting expenses.-React.Small form to manage a weekly / monthly budget, adding and subtracting expenses.-React.</p>
+                  <p>Small form to manage a weekly / monthly budget, adding and subtracting expenses.React-Hooks</p>
                   <div className="icons">
                 
                 <a className="icon" href="https://github.com/cris-perez93/Presupuesto" target="_blank" rel="noreferrer"><i className="fab fa-github" aria-hidden="true"></i></a>
